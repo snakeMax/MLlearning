f1c8c1432f975f2df286b1c41ce4df21012e7f50
snakeMax <superknutsen@gmail.com>
2025-06-07 14:42:53 +0200

    First push of model Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+//Load sample data
+using MLlearning;
+
+
+static void Predict(string? LTpredictInput)
+{
+    var sampleData = new SentimentModel.ModelInput()
+    {
+        Col0 = $"{LTpredictInput}",
+    };
+
+    //Load model and predict output
+    var result = SentimentModel.Predict(sampleData);
+
+    var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+    Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+    Console.Write("Press any key to continue...");
+    Console.ReadKey();
+}
+
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Welcome to the first Sentiment Model!");
+    Console.WriteLine("-------------------------------------");
+    Console.WriteLine("1: Predict sentiment");
+    Console.WriteLine("2: Exit");
+    Console.Write(":> ");
+    var input = Console.ReadLine();
+    if (input == "1")
+    {
+        Console.Write("Write what to predict:> ");
+        var LTpredict = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine($"Sentence: {LTpredict}");
+        Console.WriteLine("");
+        Predict(LTpredict);
+        
+
+    } else if (input == "2")
+    {
+        Environment.Exit(0);
+    } else
+    {
+        Console.WriteLine("Invalid");
+    }
+
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MLlearning
+
+# My firswt fully functional sentiment model based on Microsoft's Yelp review dataset provided from a tutorial
+
+This model has been tested with multiple different sentences, and can with over 80% acuracy determine if a review is positive or negative.
